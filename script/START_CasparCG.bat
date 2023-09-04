3f1914964c51fc8ebf694ba53a4ae8a7cd2e0aa2
ComingHOME Videotechnik <churchtool.admin@cominghome-darmstadt.de>
2023-09-04 22:29:41 +0200

    Stage Kontrollmonitor und Bugfixes
    Conflicts mit C gelöst

--- script/START_CasparCG.bat
+++ script/START_CasparCG.bat
@@ -12,6 +12,11 @@ IF NOT %ERRORLEVEL%==0 (
   start "NDI-Monitor 2" /min "%ProgramFiles%\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" Beamer
 )
 
+curl -f -s 127.0.0.1:82/v1/configuration
+IF NOT %ERRORLEVEL%==0 (
+  start "NDI-Monitor 3" /min "%ProgramFiles%\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" STAGE
+)
+
 if not exist "%mePath%..\temp" mkdir "%mePath%..\temp"
 cd "%mePath%..\temp"
 start /min ..\casparcg-server\scanner.exe --caspar.config ..\config\casparcg-server.config --paths.ffmpeg ..\casparcg-server\ffmpeg.exe --paths.ffprobe ..\casparcg-server\ffprobe.exe
