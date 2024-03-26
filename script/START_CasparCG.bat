3c237f633a1453cc75d8df23d0bcbbe5047b67fe
Michael Budnick <michael.budnick@all4net.de>
2024-03-26 07:48:43 +0100

    NDI Kontrolle Beamer entfernt

--- script/START_CasparCG.bat
+++ script/START_CasparCG.bat
@@ -7,11 +7,6 @@ IF NOT %ERRORLEVEL%==0 (
   start "NDI-Monitor 1" /min "%ProgramFiles%\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" Multiview
 )
 
-curl -f -s 127.0.0.1:81/v1/configuration
-IF NOT %ERRORLEVEL%==0 (
-  start "NDI-Monitor 2" /min "%ProgramFiles%\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" Beamer
-)
-
 if not exist "%mePath%..\temp" mkdir "%mePath%..\temp"
 cd "%mePath%..\temp"
 start /min ..\casparcg-server\scanner.exe --caspar.config ..\config\casparcg-server.config --paths.ffmpeg ..\casparcg-server\ffmpeg.exe --paths.ffprobe ..\casparcg-server\ffprobe.exe
