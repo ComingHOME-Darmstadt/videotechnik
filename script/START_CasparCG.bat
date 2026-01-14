bc47310b8a0e7a081fc07f13e9c00d6032dfc417
Phillipp <Phillippneuhaus@web.de>
2026-01-14 18:28:40 +0100

    Fix START_CasparCG.bat für abweichendes Laufwerk

--- script/START_CasparCG.bat
+++ script/START_CasparCG.bat
@@ -1,6 +1,7 @@
 @echo off
 set me=%~f0
-set mePath=%~dp0
+set mePath=%~p0
+set meDrive=%~d0
 
 curl -f -s 127.0.0.1:80/v1/configuration
 IF NOT %ERRORLEVEL%==0 (
@@ -12,9 +13,11 @@ IF NOT %ERRORLEVEL%==0 (
   start "NDI-Monitor 2" /min "%ProgramFiles%\NDI\NDI 6 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" Controlview
 )
 
+%meDrive%
+
 if not exist "%mePath%..\temp" mkdir "%mePath%..\temp"
-cd "%mePath%..\temp"
-start /min ..\casparcg-server\scanner.exe --caspar.config ..\config\casparcg-server.config --paths.ffmpeg ..\casparcg-server\ffmpeg.exe --paths.ffprobe ..\casparcg-server\ffprobe.exe
+pushd "%mePath%..\temp"
+start /min %mePath%..\casparcg-server\scanner.exe --caspar.config ..\config\casparcg-server.config --paths.ffmpeg ..\casparcg-server\ffmpeg.exe --paths.ffprobe ..\casparcg-server\ffprobe.exe
 
 timeout /t 1
 
@@ -23,3 +26,5 @@ start /min %mePath%casparcg_auto_restart.bat
 timeout /t 1
 
 call %mePath%startVideoMonitor.bat
+
+popd
